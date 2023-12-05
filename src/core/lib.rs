829e681f9461796371f4b651b452195f4da049d3
Brett Langdon <brett.langdon@datadoghq.com>
2023-12-05 17:23:57 -0500

    make message bus a global API instead of an instance

--- src/core/lib.rs
+++ src/core/lib.rs
@@ -3,7 +3,22 @@ mod message_bus;
 use pyo3::prelude::*;
 
 #[pymodule]
-fn _core(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_class::<message_bus::MessageBus>()?;
+fn _core(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    let msg_bus_module = PyModule::new(py, "message_bus")?;
+    msg_bus_module.add_function(wrap_pyfunction!(message_bus::on, msg_bus_module)?);
+    msg_bus_module.add_function(wrap_pyfunction!(message_bus::on_all, msg_bus_module)?);
+    msg_bus_module.add_function(wrap_pyfunction!(message_bus::remove, msg_bus_module)?);
+    msg_bus_module.add_function(wrap_pyfunction!(message_bus::reset, msg_bus_module)?);
+    msg_bus_module.add_function(wrap_pyfunction!(
+        message_bus::has_listeners,
+        msg_bus_module
+    )?);
+    msg_bus_module.add_function(wrap_pyfunction!(message_bus::dispatch, msg_bus_module)?);
+    msg_bus_module.add_function(wrap_pyfunction!(
+        message_bus::dispatch_with_results,
+        msg_bus_module
+    )?);
+
+    m.add_submodule(msg_bus_module)?;
     Ok(())
 }
--- src/core/message_bus.rs
+++ src/core/message_bus.rs
@@ -1,83 +1,113 @@
+use once_cell::sync::Lazy;
 use pyo3::prelude::*;
 use pyo3::types::PyFunction;
 use pyo3::types::PyList;
+use pyo3::types::PyNone;
 use pyo3::types::PyTuple;
 use std::collections::HashMap;
+use std::sync::Mutex;
 
-#[pyclass(module = "ddtrace.internal._core")]
-pub struct MessageBus {
-    listeners: HashMap<String, Vec<Py<PyFunction>>>,
-    raise_errors: bool,
+static EVENT_LISTENERS: Lazy<Mutex<HashMap<String, Vec<Py<PyFunction>>>>> =
+    Lazy::new(|| Mutex::new(HashMap::new()));
+static GLOBAL_LISTENERS: Lazy<Mutex<Vec<Py<PyFunction>>>> = Lazy::new(|| Mutex::new(Vec::new()));
+
+#[pyfunction]
+pub fn has_listeners(event_id: String) -> PyResult<bool> {
+    match EVENT_LISTENERS.lock().unwrap().get(&event_id) {
+        Some(v) => Ok(!v.is_empty()),
+        None => Ok(false),
+    }
 }
 
-#[pymethods]
-impl MessageBus {
-    #[new]
-    fn new(raise_errors: bool) -> Self {
-        Self {
-            listeners: HashMap::new(),
-            raise_errors: raise_errors,
+#[pyfunction]
+pub fn on(event_id: String, callback: Py<PyFunction>) -> PyResult<()> {
+    let mut listeners = EVENT_LISTENERS.lock().unwrap();
+
+    if let Some(l) = listeners.get_mut(&event_id) {
+        if !l.iter().any(|f| f.is(&callback)) {
+            l.insert(0, callback);
+        }
+    } else {
+        let mut v: Vec<Py<PyFunction>> = Vec::new();
+        v.push(callback);
+        listeners.insert(event_id, v);
+    }
+
+    Ok(())
+}
+
+#[pyfunction]
+pub fn on_all(callback: Py<PyFunction>) -> PyResult<()> {
+    let mut listeners = GLOBAL_LISTENERS.lock().unwrap();
+    if !listeners.iter().any(|f| f.is(&callback)) {
+        listeners.insert(0, callback);
+    }
+
+    Ok(())
+}
+
+#[pyfunction]
+pub fn reset() -> PyResult<()> {
+    EVENT_LISTENERS.lock().unwrap().clear();
+    GLOBAL_LISTENERS.lock().unwrap().clear();
+
+    Ok(())
+}
+
+#[pyfunction]
+pub fn remove(event_id: String, callback: Py<PyFunction>) -> PyResult<()> {
+    if let Some(l) = EVENT_LISTENERS.lock().unwrap().get_mut(&event_id) {
+        l.retain(|f| !f.is(&callback));
+    }
+
+    Ok(())
+}
+
+#[pyfunction]
+pub fn dispatch(py: Python<'_>, event_id: String, args: &PyTuple) -> PyResult<()> {
+    if let Some(l) = EVENT_LISTENERS.lock().unwrap().get(&event_id) {
+        for f in l {
+            f.call1(py, args);
         }
     }
 
-    #[pyo3(signature=(event_id))]
-    fn has_listeners(&self, event_id: String) -> PyResult<bool> {
-        Ok(self.listeners.get(&event_id).is_some())
+    let listeners = GLOBAL_LISTENERS.lock().unwrap();
+    for f in listeners.iter() {
+        f.call1(py, (event_id.clone(), args));
     }
 
-    #[pyo3(signature=(event_id, callback))]
-    fn on(&mut self, event_id: String, callback: Py<PyFunction>) -> PyResult<()> {
-        if let Some(v) = self.listeners.get_mut(&event_id) {
-            v.insert(0, callback);
-        } else {
-            let mut v: Vec<Py<PyFunction>> = Vec::new();
-            v.push(callback);
-            self.listeners.insert(event_id, v);
-        }
-        Ok(())
-    }
+    Ok(())
+}
 
-    #[pyo3(signature=())]
-    fn reset(&mut self) -> PyResult<()> {
-        self.listeners.clear();
-        Ok(())
-    }
+#[pyfunction]
+pub fn dispatch_with_results<'py>(
+    py: Python<'py>,
+    event_id: String,
+    args: &PyTuple,
+) -> PyResult<(&'py PyList, &'py PyList)> {
+    let none = PyNone::get(py);
+    let results = PyList::empty(py);
+    let exceptions = PyList::empty(py);
 
-    #[pyo3(signature=(event_id, callback))]
-    fn remove(&mut self, event_id: String, callback: Py<PyFunction>) -> PyResult<()> {
-        if let Some(v) = self.listeners.get_mut(&event_id) {
-            v.retain(|f| !f.is(&callback))
-        }
-        Ok(())
-    }
-
-    #[pyo3(signature=(event_id, args))]
-    fn dispatch<'py>(
-        &self,
-        py: Python<'py>,
-        event_id: String,
-        args: &PyTuple,
-    ) -> PyResult<(&'py PyList, &'py PyList)> {
-        if let Some(v) = self.listeners.get(&event_id) {
-            let mut results: Vec<Py<PyAny>> = vec![];
-            let mut exceptions: Vec<PyErr> = vec![];
-
-            for f in v {
-                match f.call1(py, args) {
-                    Err(e) => {
-                        if self.raise_errors {
-                            return Err(e);
-                        }
-                        exceptions.push(e);
-                    }
-                    Ok(r) => {
-                        results.push(r);
-                    }
+    if let Some(l) = EVENT_LISTENERS.lock().unwrap().get(&event_id) {
+        for f in l {
+            match f.call1(py, args) {
+                Err(e) => {
+                    results.append(none);
+                    exceptions.append(e);
+                }
+                Ok(r) => {
+                    results.append(r);
+                    exceptions.append(none);
                 }
             }
-            return Ok((PyList::new(py, results), PyList::new(py, exceptions)));
         }
-
-        Ok((PyList::empty(py), PyList::empty(py)))
     }
+
+    let listeners = GLOBAL_LISTENERS.lock().unwrap();
+    for f in listeners.iter() {
+        f.call1(py, (event_id.clone(), args));
+    }
+
+    Ok((results, exceptions))
 }
