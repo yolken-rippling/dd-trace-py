d0a239aa853f38f051486ae6827f634f0d1867ac
Brett Langdon <brett.langdon@datadoghq.com>
2024-04-11 18:22:08 -0400

    fix isort import order

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ import sysconfig
 import tarfile
 
 import cmake
+from setuptools_rust import Binding
+from setuptools_rust import RustExtension
 
 
 from setuptools import Extension, find_packages, setup  # isort: skip
 from setuptools.command.build_ext import build_ext  # isort: skip
 from setuptools.command.build_py import build_py as BuildPyCommand  # isort: skip
-from setuptools_rust import Binding
-from setuptools_rust import RustExtension
 from pathlib import Path  # isort: skip
 from pkg_resources import get_build_platform  # isort: skip
 from distutils.command.clean import clean as CleanCommand  # isort: skip
